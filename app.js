997f256938ec592d6d6d78e1009b7e7c0b3eb659
Bahriddin <Bahriddin>
2023-10-26 11:11:23 +0500

    test

---
+++ app.js
@@ -0,0 +1,11 @@
+// let mevalar = ["olma", "nok", "gilos" , "olcha"];
+// console.log(mevalar.shift);
+// console.log(mevalar);
+
+// 1) Ism va tugilgan kun sorab Hurmatli {Ism}, siz {yil}yil {oy}oy {kun}kun {soat}soat yashabsiz Namuna: 10 yil, 8oy, 22kun ✅ 10 yil, 128oy, 3862kun ❌
+
+// let ism = prompt("Ismingizni kiriting?");
+// let day = prompt("Nechanchi yil tug'ulgansiz?");
+// let hozir = new Date();
+// console.log(hozir.get)
+
